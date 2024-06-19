7593800bf51806a30c66e484eb8785d7781664ae
Ismael Bejarano <ibejarano@manas.tech>
2024-06-19 20:45:17 -0300

    Start with execution witness

--- src/core/verkle_state.cr
+++ src/core/verkle_state.cr
@@ -1,7 +1,13 @@
+require "../common/types.cr"
+
 module Pampero
   class VerkleState
+    def initialize
+      @state = Hash(Bytes32, Bytes32).new
+    end
+
     def read_key(key : Bytes32) : Bytes32
-      Bytes32.new(0u8)
+      @state.fetch(key, Bytes32.new(0u8))
     end
   end
 end
