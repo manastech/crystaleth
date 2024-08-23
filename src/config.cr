3582c28934c621a39fc488a419f5066cc17471d7
Beta Ziliani <beta@manas.tech>
2024-08-23 14:02:10 -0300

    Fixed wrong accessor to env

--- src/config.cr
+++ src/config.cr
@@ -1,10 +1,9 @@
 module Pampero
-
   class Config
     getter beacon_node : String
 
     def initialize
-      @beacon_node = ENV["BEACON_NODE"] || ""
+      @beacon_node = ENV["BEACON_NODE"]? || ""
     end
   end
 end
